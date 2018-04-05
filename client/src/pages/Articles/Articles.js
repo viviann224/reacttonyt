4d7f75eac2ba3131e98b771d94c8820ddda714b2
viviann224 <viviann224@gmail.com>
2018-04-05 16:56:13 -0500

    updated Articles.js page wit with cleaned up comments for saving articles and api request

--- client/src/pages/Articles/Articles.js
+++ client/src/pages/Articles/Articles.js
@@ -4,70 +4,64 @@ import Jumbotron from "../../components/Jumbotron";
 import API from "../../utils/API";
 import { H1, H3, H4 } from '../../components/Headings';
 import { Col, Row, Container } from "../../components/Grid";
-//import {  ListItem } from "../../components/List";
 import { Form, Input, FormBtn, FormGroup, Label } from "../../components/Form";
 import { Panel, PanelHeading, PanelBody } from '../../components/Panel';
-//import Detail from "../SavedArticles";
 
-class Articles extends Component {
-  state = {
+class Articles extends Component 
+{ //variables initalized with state properties
+  state = 
+  {
     articles: [],
     Article: "",
     startyear: "",
     endyear: "",
-    page: "0",//page of search results
-    previousSearch: {},//previous search term saved after search completed
-    noarticles: false,//boolean used as flag for conditional rendering
-    savedArticles: {}
+    page: "0",            //pages of results
+    previousSearch: {},   //keep track of the previous state query 
+    noarticles: false,    //boolean variable to determine to display noarticles found
+    savedArticles: {}     //keep track of specific saved articles
   };
 
-  //function to save an article
+  //function to save an article from the api call
   saveArticle = (result) => 
   {
-    //creating new article object
-    let newResult = {
+    //creating new article object and passing date, title, url, and summary from displayed search
+    let newResult = 
+    {
       date: result.pub_date,
       title: result.headline.main,
       url: result.web_url,
       summary: result.snippet
     }
-    //console.log("saved area: "+result.pub_date + result.headline.main+ result.web_url);
-    //calling the API
+    //calling the API to store new saved article
     API
       .saveArticle(newResult)
       .then(articles => 
       {
-        //removing the saved article from the articles in state
+        //once the article is saved go ahead and filter out all the unsaved articles for user
         let unsavedArticles = this.state.articles.filter(result => result.headline.main !== newResult.title)
         this.setState({articles: unsavedArticles})
-        //when state updated go ahead (referesh)/read the update data
-        
-
       })
       .catch(err => console.log(err));
-
+      //then go ahead and reload and display all the articles
       this.loadArticles();
-      //console.log("finished saving");
   }
 
+//once everything loads go ahead and display (on the first user scession)
  componentWillMount() 
   {  this.loadArticles();}
 
-  //function reads requests from previously saved articles (from the db) and returns the info 
+  //loadArticles is a function which reads requests from previously saved articles (from the db) and returns the info 
   loadArticles = () => 
   {
     API
       .getArticles()
-      .then(results => {
-
-        this.setState({savedArticles: results.data})
-      })
-
+      .then(results => 
+      {   this.setState({savedArticles: results.data}) })
   };
 
   handleInputChange = event => 
   {
-
+    //when there is a change to the field, the input for title, endyear, or startyear is updated
     const { name, value } = event.target;
     this.setState({
       [name]: value
@@ -88,23 +82,23 @@ class Articles extends Component {
   //function that requests the NYT API
   getHeadlines = (query) => 
   {
-    //clearing the results array if the user changes search terms
+    //if the user changed the topic, endyear, or the start year go ahead and read out new articles
     if (query.topic !== this.state.previousSearch.topic ||
         query.endyear !==this.state.previousSearch.endyear ||
         query.startyear !==this.state.previousSearch.startyear) 
-    {
+    { //go ahead and empty out the last search
       this.setState({articles: []})
-    }
+    } //update the new each fields 
     let { topic, startyear, endyear } = query
 
     let queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?sort=newest&page=${this.state.page}`
     let key = `&api-key=ac37023831a046ad9447acf3101b4c79`
 
-    //removing spaces and building the query url conditionally
-    //based on presence of optional search terms
+    //removes the special spaces in the search query url
     if(topic.indexOf(' ')>=0){
       topic = topic.replace(/\s/g, '+');
     }
+    //based on the query go ahead and append each parameter to the queryUrl
     if (topic){
       queryUrl+= `&fq=${topic}`
     }
@@ -116,14 +110,12 @@ class Articles extends Component {
     }
     queryUrl+=key;
 
-    //calling the API
+    //once queryUrl build go ahead and pass an api request to nyt
     API
       .queryNYT(queryUrl)
       .then(articles => {
-        //console.log("the articles: " +articles.data.response.docs);
-          //concatenating new results to the current state of results.  If empty will just show results,
-          //but if search was done to get more, it shows all results.  Also stores current search terms
-          //for conditional above, and sets the noarticles flag for conditional rendering of components below
+          //once query request finish store the current scession to previous search, 
+          //and clear out the rest of fields for next
           this.setState({
             articles: [...this.state.articles, ...articles.data.response.docs],
             previousSearch: query,
@@ -137,9 +129,7 @@ class Articles extends Component {
             this.state.articles.length === 0 ? this.setState({noarticles: true}) : this.setState({noarticles: false})
           });
       })
-      //.catch(err=> console.log(err))
   }
-
   render() {
     return (
       <Container fluid>
@@ -148,7 +138,6 @@ class Articles extends Component {
             <Jumbotron>
               <H1 className='page-header text-center'>REACT to NEW YORK TIMES</H1>
               <H4 className='text-center'>Reacting to all the best articles one save at a time</H4>
-            
             </Jumbotron>
             <Panel>
               <PanelHeading>
@@ -162,33 +151,26 @@ class Articles extends Component {
                       name='topic'
                       value={this.state.name}
                       onChange={this.handleInputChange}
-                      
-                      
                       placeholder='Topic of Interest'
                     />
                   </FormGroup>
                   <FormGroup >
                     <Label htmlFor="startyear">Choose a beginning date to search (optional):</Label>
                     <Input
-                    
-                     name='startyear'
-                    value={this.state.startyear}
+                      name='startyear'
+                      value={this.state.startyear}
                       onChange={this.handleInputChange}
                       type='date'
-                     
-                      
                       placeholder='Starting Year'
                     />
                   </FormGroup>
                   <FormGroup >
                     <Label htmlFor="endyear">Choose an end date to search for (optional):</Label>
                     <Input
-                    name='endyear'
+                      name='endyear'
                       value={this.state.endyear}
                       onChange={this.handleInputChange}
                       type='date'
-                      
-                      
                       placeholder='Ending Year'
                     />
                   </FormGroup>
@@ -222,8 +204,7 @@ class Articles extends Component {
                           />
                         )
                       )
-                    }
-                     
+                    }  
                   </PanelBody>
                 </Panel>
               ) : ''
